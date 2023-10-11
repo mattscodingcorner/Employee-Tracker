f7e07c37fbef756b0ac5c1bfc33b4a7fd350eb09
unknown <matt.j.velazquez@gmail.com>
2023-10-10 18:28:26 -0700

    updated index logic

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ const showPrompt = () => {
                 'Add Role',
                 'View All Departments',
                 'Add Department',
+                'View All Employees By Department',
                 'Quit'
             ]
         }
@@ -57,6 +58,9 @@ const showPrompt = () => {
             case 'Add Department':
                 addDepartment();
                 break;
+            case 'View All Employees By Department':
+                viewAllEmployeesByDepartment();
+                break;    
                 case 'Quit':
                     connection.end();
                     break;
@@ -119,3 +123,140 @@ const addEmployee = () => {
         })
     });
 };
+
+const updateEmployeeRole = () => {
+    connection.query('SELECT * FROM role', function (err, res) {
+        if (err) throw err;
+        connection.query('SELECT * FROM employee', function (err, res2) {
+            if (err) throw err;
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'employee',
+                    message: 'Which employee would you like to update?',
+                    choices: res2.map(employee => employee.first_name + ' ' + employee.last_name)
+                },
+                {
+                    type: 'list',
+                    name: 'role',
+                    message: 'What is the new role of the employee?',
+                    choices: res.map(role => role.title)
+                }
+            ]).then((answer) => {
+                let roleID = res.find(role => role.title === answer.role).id;
+                let employeeID = res2.find(employee => employee.first_name + ' ' + employee.last_name === answer.employee).id;
+                connection.query('UPDATE employee SET ? WHERE ?',
+                    [
+                        {
+                            role_id: roleID
+                        },
+                        {
+                            id: employeeID
+                        }
+                    ],
+                    function (err, res) {
+                        if (err) throw err;
+                        console.log('Employee updated!');
+                        showPrompt();
+                    })
+            })
+        })  
+    });
+}
+
+const viewAllRoles = () => {
+    connection.query('SELECT * FROM role', function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        showPrompt();
+    })
+};
+const addRole = () => {
+    connection.query('SELECT * FROM department', function (err, res) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'role',
+                message: 'What is the name of the role?'
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'What is the salary of the role?'
+            },
+            {
+                type: 'list',
+                name: 'department',
+                message: 'What is the department of the role?',
+                choices: res.map(department => department.name)
+            }
+        ]).then((answer) => {
+            let departmentID = res.find(department => department.name === answer.department).id;
+            connection.query('INSERT INTO role SET ?',
+                {
+                    title: answer.role,
+                    salary: answer.salary,
+                    department_id: departmentID
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    console.log('Role added!');
+                    showPrompt();
+                })
+        })
+    });
+};
+const viewAllDepartments = () => {
+    connection.query('SELECT * FROM department', function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        showPrompt();
+    })
+};
+const addDepartment = () => {
+    connection.query('SELECT * FROM department', function (err, res) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'department',
+                message: 'What is the name of the department?'
+            }
+        ]).then((answer) => {
+            connection.query('INSERT INTO department SET ?',
+                {
+                    name: answer.department
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    console.log('Department added!');
+                    showPrompt();
+                })
+        })
+    });
+};
+/* const viewAllEmployeesByDepartment = () => {
+    connection.query('SELECT * FROM department', function (err, res) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'department',
+                message: 'Which department would you like to view?',
+                choices: res.map(department => department.name)
+            }
+        ]).then((answer) => {
+            let departmentID = res.find(department => department.name === answer.department).id;
+            connection.query('SELECT * FROM employee WHERE ?',
+                {
+                    department_id: departmentID
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    console.table(res);
+                    showPrompt();
+                })
+        })
+    });
+}; */
